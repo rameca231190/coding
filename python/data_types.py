6e5dac53abbbfb1ee618b29f8e42ec18c4b8ee62
rameca231190 <romapereverziev@gmail.com>
2021-02-24 19:02:41 -0500

    Python code

---
+++ python/data_types.py
@@ -0,0 +1,30 @@
+# Data types in python (strings, numbers)
+# string (plain text)
+print("Giraffe Academy")
+
+#Print string as a new line
+print("Giraffe\n Academy")
+
+# \ Gives ability to put quotation marks to string
+print("Giraffe\"Academy")
+
+#Add one string to another.
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+
+#Print as upper or lower case
+print(phrase.upper())
+print(phrase.lower())
+
+#Print string with len function which wil show you amount of characters in string
+print(len(phrase))
+
+#Print character in string by position (position starting from 0)
+print(phrase[0])
+
+#Return an index from string
+print(phrase.index('a'))
+
+
+
+# numbers (when storing numbers you don't need quotation marks)
---
+++ python/variable.py
@@ -0,0 +1,7 @@
+# variable
+character_name = "John"
+#variable
+character_age = "35"
+
+print("There once was man named " + character_name + ",")
+print("he was " + character_age + " years old. ")
