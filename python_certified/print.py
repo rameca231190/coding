f75fec004863fce52b3aea462603901fe45877ff
rameca231190 <romapereverziev@gmail.com>
2021-04-06 11:36:03 -0400

    more python scripts

---
+++ python_certified/print.py
@@ -0,0 +1,9 @@
+#print("Hello", "future", "python", "programmer")
+#print("So", "enjoing Python ?", sep=", ", end=" Aloha")
+
+# sep this is to add some character after each print string, and end add something to the end of it.
+#print("h", "d", "l", sep=", ", end=", Aloha")
+
+
+#print('Name' + 25)
+print("Hello \"Python\" world")
