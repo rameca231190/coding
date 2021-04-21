e0c2530bd38d982d3aeab1d52cb294b41048d8ce
rameca231190 <romapereverziev@gmail.com>
2021-04-20 21:51:04 -0400

    more python scripts

---
+++ python_certified/literals.py
@@ -0,0 +1,42 @@
+#x = 'abcd'
+#for i in range(len(x)):
+#    print("hello")
+
+
+#x = "2021"
+#for i in x:
+#  print(i)
+
+is_hungry = False
+#means if not true
+#if(not is_hungry):
+#  print("You are not hungry")
+#else:
+#  print("You are hungry")
+
+#a = 20
+#b = 5
+#print("a & b =", a & b)
+
+
+
+#list1 = [1, 2, 3, 4]
+#for index, j in enumerate(list1):
+#     print(j, index)
+
+
+
+letters
+
+
+
+
+
+
+
+
+
+
+
+
+
